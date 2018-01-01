ca2d2ff2c1ac972db3cf14bb7a3e4ac07855971e
Akai-Kumako <e1433@s.akashi.ac.jp>
2018-01-02 02:32:31 +0900

    6章 完

--- 59.py
+++ 59.py
@@ -1,19 +1,13 @@
 #59. S式の解析
 
 import xml.etree.ElementTree as ET
+import re
 
 root = ET.parse("nlp.txt.xml")
-
-nsubj = {}
-dobj = {}
+phrase = re.compile("\(NP\s(.*?)\)\)")
+noun = re.compile("\([A-Z]+\s(.*?)\)")
 
 for sentence in root.iterfind("./document/sentences/sentence"):
-  dependence = sentence.iterfind('./dependencies[@type="collapsed-dependencies"]/dep')  
-  for dep in dependence:
-    if dep.get("type") == "nsubj":
-      nsubj[dep.find("./governor").text] = dep.find("./dependent").text
-    if dep.get("type") == "dobj":
-      dobj[dep.find("./governor").text] = dep.find("./dependent").text
-  for predicate in nsubj.keys():
-    if predicate in dobj.keys():
-      print("{}\t{}\t{}".format(nsubj[predicate], predicate, dobj[predicate]))
+  parse = sentence.findtext("parse")
+  for n in phrase.findall(parse):
+    print(re.sub("\([A-Z]+\s", "", " ".join(noun.findall(n + ")"))))
