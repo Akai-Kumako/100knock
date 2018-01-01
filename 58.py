e9f8b7f6a0942dcf33ce122600b3f80051c5e78a
Akai-Kumako <e1433@s.akashi.ac.jp>
2018-01-02 01:30:23 +0900

    6章もうちょい

---
+++ 58.py
@@ -0,0 +1,19 @@
+#58. タプルの抽出
+
+import xml.etree.ElementTree as ET
+
+root = ET.parse("nlp.txt.xml")
+
+nsubj = {}
+dobj = {}
+
+for sentence in root.iterfind("./document/sentences/sentence"):
+  dependence = sentence.iterfind('./dependencies[@type="collapsed-dependencies"]/dep')  
+  for dep in dependence:
+    if dep.get("type") == "nsubj":
+      nsubj[dep.find("./governor").text] = dep.find("./dependent").text
+    if dep.get("type") == "dobj":
+      dobj[dep.find("./governor").text] = dep.find("./dependent").text
+  for predicate in nsubj.keys():
+    if predicate in dobj.keys():
+      print("{}\t{}\t{}".format(nsubj[predicate], predicate, dobj[predicate]))
