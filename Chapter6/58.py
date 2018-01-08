16dd08b86091f39f567a17a29685ddbdcfc11579
Akai-Kumako <e1433@s.akashi.ac.jp>
2018-01-08 18:33:59 +0900

    #58 原因が分かった 解決

--- Chapter6/58.py
+++ Chapter6/58.py
@@ -13,11 +13,11 @@ for sentence in root.iterfind("./document/sentences/sentence"):
     gove = dep.find("./governor").text
     depe = dep.find("./dependent").text
     idx = dep.find("./governor").get("idx")
-    pred[idx] = gove
+    pred[int(idx)] = gove
     if dep.get("type") == "nsubj":
-      nsubj[idx] = depe
+      nsubj[int(idx)] = depe
     if dep.get("type") == "dobj":
-      dobj[idx] = depe
-  for idx in nsubj.keys():
+      dobj[int(idx)] = depe
+  for idx in pred.keys():
     if idx in dobj.keys() and idx in nsubj.keys():
       print("{}\t{}\t{}".format(nsubj[idx], pred[idx], dobj[idx]))
