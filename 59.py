c860e8efd3fcc09bee5798b397f6bbe833176c0c
Akai-Kumako <e1433@s.akashi.ac.jp>
2018-01-08 17:26:17 +0900

    #59 間違ってたとこ変更

--- 59.py
+++ 59.py
@@ -7,10 +7,24 @@ root = ET.parse("nlp.txt.xml")
 phrase = re.compile("\(NP\s(.*?)\)\)")
 noun = re.compile("\([A-Z]+\s(.*?)\)")
 
-text = []
-
 for sentence in root.iterfind("./document/sentences/sentence"):
   parse = sentence.findtext("parse")
-  for n in phrase.findall(parse):
-    
-    print(re.sub("\([A-Z]+\s", "", " ".join(noun.findall(n + ")"))))
+  ss = parse.split(" ")
+  now = 0
+  for s in ss:
+    if s == "":
+      continue    
+    elif s[0] == "(":
+      if s == "(NP":
+        lsb = 0
+        rsb = 0
+        for w in ss[now + 1:]:
+          if w == "" or lsb - rsb < 0:
+            continue
+          elif w[0] == "(":
+            lsb += 1
+          elif w[-1] == ")":
+            rsb += w.count(")")
+            print(w[:len(w)-w.count(")")], end = " ")
+        print()
+    now += 1
