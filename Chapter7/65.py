d7fe69ac09e91741cc7aa9719a2dadabd066056f
Akai-Kumako <e1433@s.akashi.ac.jp>
2018-01-15 12:44:58 +0900

    使うデータベース間違ってた

--- Chapter7/65.py
+++ Chapter7/65.py
@@ -7,7 +7,7 @@ import json
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 
-db = client.nlp_database
+db = client.kyoko
 co = db.nlp_collection
 
 for data in co.find({"name": "Queen"}):
