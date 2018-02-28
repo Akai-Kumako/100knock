442f4ddad9e7df6739f49ddf3d97a616b34c941b
Akai-Kumako <e1433@s.akashi.ac.jp>
2018-02-28 11:28:49 +0900

    終わらせれてなかった〜

--- Chapter2/13.py
+++ Chapter2/13.py
@@ -5,9 +5,11 @@ a = open("col1.txt", "r")
 b = open("col2.txt", "r")
 f = open("col.txt", "w")
 
-text = "\t".join([a.read(), b.read()])
-text = text.replace("\n", "")
-f.write(text)
+col1 = a.readlines()
+col2 = b.readlines()
+
+for data1, data2 in zip(col1, col2):
+  f.writelines(data1.replace("\n", "") + "\t" + data2);
 
 a.close()
 b.close()
